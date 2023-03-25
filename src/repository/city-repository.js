81c44d87f819179ff2e1477ed4f06e952eefe61f
AmitJalal <amitjalal53@gmail.com>
2023-03-25 22:04:44 +0530

    added crud functionalities to city-repository layer

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,9 +3,12 @@ const { City } = require("../models");
 class CityRepository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ name });
+      const city = await City.create({
+        name,
+      });
       return city;
     } catch (error) {
+      console.log("Something went wrong in the repository layer");
       throw { error };
     }
   }
@@ -17,7 +20,52 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getAllCities(filter) {
+    // filter can be empty also
+    try {
+      if (filter.name) {
+        const cities = await City.findAll({
+          where: {
+            name: {
+              [Op.startsWith]: filter.name,
+            },
+          },
+        });
+        return cities;
+      }
+      const cities = await City.findAll();
+      return cities;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
       throw { error };
     }
   }
